6bc77280a48a4e2137ad02e40a5ac59bfd741f1e
blezDev <msaikrishnapatnaik@gmail.com>
2024-02-27 23:28:35 +0530

    Modified index.html, shop-details.html

--- js/main.js
+++ js/main.js
@@ -228,3 +228,126 @@
 const userName = document.getElementById("user-login-name");
 userName.textContent.replaceAll("Login","");
 userName.textContent = localStorage.getItem("user_email");
+
+const almond = document.getElementById("almond-pic");
+almond.addEventListener("click",()=>{
+    let id = "171686";
+    localStorage.setItem("itemID",id);
+    localStorage.setItem("itemName","Almond");
+    window.location = "shop-details.html"
+});
+
+const banana = document.getElementById("banana-pic");
+
+banana.addEventListener("click",()=>{
+    let id = "1105073";
+    localStorage.setItem("itemID",id);
+    localStorage.setItem("itemName","Banana");
+    window.location = "shop-details.html"
+});
+
+const chicken = document.getElementById("chicken-pic");
+chicken.addEventListener("click",()=>{
+    let id = "2514746";
+    localStorage.setItem("itemID",id);
+    localStorage.setItem("itemName","Chicken");
+    window.location = "shop-details.html"
+});
+
+const watermelon = document.getElementById("watermelon-pic");
+
+watermelon.addEventListener("click",()=>{
+
+});
+
+const grapes = document.getElementById("grapes-pic");
+
+grapes.addEventListener("click",()=>{
+   const id = "2346413";
+    localStorage.setItem("itemID",id);
+    localStorage.setItem("itemName","Grapes");
+    window.location = "shop-details.html"
+});
+
+const fish = document.getElementById("fish-pic");
+
+fish.addEventListener("click",()=>{
+    const id = "333374";
+    localStorage.setItem("itemID",id);
+    localStorage.setItem("itemName","Fish");
+    window.location = "shop-details.html"
+});
+
+const mango = document.getElementById("mango-pic");
+
+mango.addEventListener("click",()=>{
+    const id = "2344737";
+    localStorage.setItem("itemID",id);
+    localStorage.setItem("itemName","Mango");
+    window.location = "shop-details.html"
+});
+
+const apple = document.getElementById("apple-pic");
+
+apple.addEventListener("click",()=>{
+    const id = "1750340";
+    localStorage.setItem("itemID",id);
+    localStorage.setItem("itemName","Apples");
+    window.location = "shop-details.html"
+});
+
+
+//shop-details
+async function fetchData() {
+    try {
+        const id = localStorage.getItem("itemID");
+        let itemName = localStorage.getItem("itemName");
+        const url = `https://api.nal.usda.gov/fdc/v1/food/${id}?api_key=AM8O0qiDqSBecgxaVyrQDSseYvz3FanDdXTMYIxf`;
+        const response = await axios.get(url);
+        const foodDetails = response.data;
+        const h3Element = document.querySelector('.product__details__text h3');
+        // Add text using innerText property
+        h3Element.innerText = itemName;
+        const itemDescription = document.getElementById("item-description");
+
+        h3Element.innerText = foodDetails.description;
+        const nutrientList = document.getElementById("nutrients-list");
+
+        foodDetails["foodNutrients"].forEach(item=>{
+            // Create a new <li> element
+            const liElement = document.createElement('li');
+
+            // Create a <b> element for bold text
+            const boldElement = document.createElement('b');
+            boldElement.textContent = item.nutrient.name + ': ';
+
+            // Create a <span> element for the value
+            const spanElement = document.createElement('span');
+            const amountValue = item.amount !== null ? item.amount : "0";
+            spanElement.innerHTML = `${amountValue} ${item.nutrient.unitName}`;
+
+            // Append the <b> and <span> elements to the <li> element
+            liElement.appendChild(boldElement);
+            liElement.appendChild(spanElement);
+
+            // Append the <li> element to the <ul> element
+            nutrientList.appendChild(liElement);
+        });
+
+
+
+
+
+
+
+        itemDescription.innerText = `Food Nutrients`
+
+        const dataType =  document.getElementById("data-type");
+        dataType.innerText = `Data Type : ${foodDetails["dataType"]}`;
+
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
